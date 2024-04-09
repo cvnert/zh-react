c911ad70bbd1d2ec48eb5c47d30664c0a8c90697
cvnert <cvnert@163.com>
2024-04-09 19:49:12 +0800

    feat: jsx实现

---
+++ packages/shared/ReactSymbols.ts
@@ -0,0 +1,4 @@
+//判断当前环境是否支持Symbol
+const supportSymbols = typeof Symbol === 'function' && Symbol.for;
+
+export const REACT_ELEMENT_TYPE = supportSymbols ? Symbol.for('react.element') : 0xeac7;
---
+++ react-demo/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const jsx = <h1>Hello, React!</h1>;
+
+console.log(React,jsx)
